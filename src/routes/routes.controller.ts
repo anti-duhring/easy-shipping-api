f496b321fa2d27a06a451e0d834fa41361e923f3
anti-duhring <mateus.limavn@gmail.com>
2023-08-29 14:50:59 -0300

    feat: Add serializer when create a new route

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -2,14 +2,16 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { RoutesSerializer } from './routes.serializer';
 
 @Controller('routes')
 export class RoutesController {
   constructor(private readonly routesService: RoutesService) {}
 
   @Post()
-  create(@Body() createRouteDto: CreateRouteDto) {
-    return this.routesService.create(createRouteDto);
+  async create(@Body() createRouteDto: CreateRouteDto) {
+    const route = await this.routesService.create(createRouteDto);
+    return new RoutesSerializer(route)
   }
 
   @Get()
